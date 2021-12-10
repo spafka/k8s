2964f43c3025f80cf325a4b1e182ea7204b5f310
spafka <spafka@163.com>
2021-12-10 16:54:16 +0800

    add jol

--- nginx.sh
+++ nginx.sh
@@ -4,7 +4,7 @@ kind: Deployment
 metadata:
   name: nginx
 spec:
-  replicas: 3
+  replicas: 1
   selector:
     matchLabels:
       run: nginx
@@ -19,19 +19,6 @@ spec:
           ports:
             - containerPort: 80
               protocol: TCP
----
-apiVersion: v1
-kind: Pod
-metadata:
-  name: nginx
-spec:
-  containers:
-    - image: nginx
-      name: pod
-      ports:
-        - name: nginx-port
-          containerPort: 80
-          protocol: TCP
 ---
 apiVersion: v1
 kind: Service
