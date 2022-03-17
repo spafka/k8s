d7e064ea46549a9cf41715ba2064bde488ff5bb1
spafka <spafka@163.com>
2022-03-17 17:10:13 +0800

    up

---
+++ a.md
@@ -0,0 +1,3 @@
+```bash
+kubect run --image=nginx 
+```
--- centos-cluster-install/install_k8s.sh
+++ centos-cluster-install/install_k8s.sh
@@ -1,105 +1,177 @@
-set -x
-#passwd root
-systemctl disable ufw && systemctl stop ufw
-apt install openssh-server -y
-systemctl start sshd
-systemctl enable  ssh
+#!/bin/bash
+# Kubernetes部署环境要求：
+#（1）一台或多台机器，操作系统CentOS 7.x-86_x64
+#（2）硬件配置：内存2GB或2G+，CPU 2核或CPU 2核+；
+#（3）集群内各个机器之间能相互通信；
+#（4）集群内各个机器可以访问外网，需要拉取镜像；
+#（5）禁止swap分区；
 
-function green_echo () {
-        local what=$*
-        echo -e "\e[1;32m[success] ${what} \e[0m"
+# 安装步骤
+#1. 安装docker
+#1.1 如果没有安装docker，则安装docker。会附带安装一个docker-compose
+#
+#2. 安装k8s
+#2.1 初始化环境
+#2.2 添加安装源
+#2.3 安装kubelet、kubectl、kubeadmin
+#2.4 安装master
+#2.5 安装网络插件
+
+set -e
+
+# 安装日志
+install_log=/var/log/install_k8s.log
+tm=$(date +'%Y%m%d %T')
+
+# 日志颜色
+COLOR_G="\x1b[0;32m"  # green
+RESET="\x1b[0m"
+
+function info(){
+    echo -e "${COLOR_G}[$tm] [Info] ${1}${RESET}"
 }
-function yellow_echo () {
-        local what=$*
-        echo -e "\e[1;33m[warnning] ${what} \e[0m"
+
+function run_cmd(){
+  sh -c "$1 | $(tee -a "$install_log")"
 }
-function red_echo () {
-        local what=$*
-        echo -e "\e[1;31m[error] ${what} \e[0m"
-        exit 1
+
+function run_function(){
+  $1 | tee -a "$install_log"
 }
-if [ -z $MASTER ]; then red_echo "设置 localIp"
-fi
 
-green_echo "set hostname"
-hostnamectl set-hostname k8s-master
+function install_docker(){
+  info "1.使用脚本自动安装docker..."
+  curl -sSL https://get.daocloud.io/docker | sh
 
+  info "2.启动 Docker CE..."
+  sudo systemctl enable docker
+  sudo systemctl start docker
 
-
-green_echo "install maven"
-apt install -y wget
-wget https://archive.apache.org/dist/maven/maven-3/3.8.2/binaries/apache-maven-3.8.2-bin.tar.gz
-tar -zxvf apache-maven-3.8.2-bin.tar.gz -C /usr/local
-ln -s /usr/local/apache-maven* /usr/local/maven
-echo "export PATH=\$PATH:/usr/local/maven/bin" >> /etc/profile
-source /etc/profile
-
-# shellcheck disable=SC1072
-if [ ! type mvn > /dev/null 2 >&1]; then
-   red_echo "mvn not intalled"
-fi
-
-sudo apt install openjdk-8-jdk -y
-if [ ! type javac >> /dev/null 2>&1]; then
-   red_echo "java not intalled"
-fi
-apt install wget curl vim git ipvsadm  dnsutils net-tools -y
-green_echo "close firewall"
-
-apt-get remove docker docker-engine docker.io containerd runc
-green_echo "installing  docker "
-apt install docker.io -y
-yellow_echo "docker "
-
-#apt-get purge -y docker-ce docker-ce-cli containerd.io
-#rm -rf /var/lib/docker
-
-cat > /etc/docker/daemon.json << ERIC
+  info "3.添加镜像加速器..."
+  if [ ! -f "/etc/docker/daemon.json" ];then
+    touch /etc/docker/daemon.json
+  fi
+  cat <<EOF > /etc/docker/daemon.json
 {
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2",
-  "storage-opts": [
-    "overlay2.override_kernel_check=true"
-  ],
-  "registry-mirrors": ["https://7uuu3esz.mirror.aliyuncs.com"],
-  "data-root": "/data/docker"
+  "registry-mirrors": [
+    "https://5ajk0rns.mirror.aliyuncs.com"
+    ]
 }
-ERIC
-
-systemctl daemon-reload
-systemctl enable docker.service
-systemctl restart docker
-
-
-yellow_echo "dokcer 状态:"
-yellow_echo $(docker ps -a)
-
-sudo apt-get update && sudo apt-get install -y ca-certificates curl software-properties-common apt-transport-https curl
-curl -s https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo tee /etc/apt/sources.list.d/kubernetes.list <<EOF
-deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
 
-sudo apt-get update
-sudo apt-mark hold kubelet kubeadm kubectl
+  info "4.重新启动服务..."
+  sudo systemctl daemon-reload
+  sudo systemctl restart docker
 
-#yellow_echo $(apt-cache madison kubeadm)
-apt  install kubelet=1.20.0-00 kubeadm=1.20.0-00 kubectl=1.20.0-00 -y
+  info "5.测试 Docker 是否安装正确..."
+  docker run hello-world
 
-# kubeadm config images list --kubernetes-version=v1.20.0
+  info "6.检测..."
+  docker info
 
+  read -p "是否安装docker-compose？默认为 no. Enter [yes/no]：" is_compose
+  if [[ "$is_compose" == 'yes' ]];then
+    info "7.安装docker-compose"
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod a+x /usr/local/bin/docker-compose
+
+    # 8.验证是否安装成功
+    info "8.验证docker-compose是否安装成功..."
+    docker-compose -v
+  fi
+}
+
+function install_k8s() {
+    info "初始化k8s部署环境..."
+    init_env
+
+    info "添加k8s安装源..."
+    add_aliyun_repo
+
+    info "安装kubelet kubeadmin kubectl..."
+    install_kubelet_kubeadmin_kubectl
+
+    info "安装kubernetes master..."
+    yum -y install net-tools
+    if [[ ! "$(ps aux | grep 'kubernetes' | grep -v 'grep')" ]];then
+      kubeadmin_init
+    else
+      info "kubernetes master已经安装..."
+    fi
+
+    info "安装网络插件flannel..."
+    install_flannel
+
+    info "去污点..."
+    kubectl taint nodes --all node-role.kubernetes.io/master-
+}
+
+# 初始化部署环境
+function init_env() {
+  info "关闭防火墙"
+  systemctl stop firewalld
+  systemctl disable firewalld
+
+  info "关闭selinux"
+  sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/g' /etc/selinux/config
+  source /etc/selinux/config
+
+  info "关闭swap（k8s禁止虚拟内存以提高性能）"
+  swapoff -a
+  sed -i '/swap/s/^\(.*\)$/#\1/g' /etc/fstab
+
+  info "设置网桥参数"
+  cat <<-EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+  sysctl --system  #生效
+  sysctl -w net.ipv4.ip_forward=1
+
+  info "时间同步"
+  yum install ntpdate -y
+  ntpdate time.windows.com
+}
+
+# 添加aliyun安装源
+function add_aliyun_repo() {
+  cat > /etc/yum.repos.d/kubernetes.repo <<- EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+}
+
+function install_kubelet_kubeadmin_kubectl() {
+  yum install kubelet-1.19.4 kubeadm-1.19.4 kubectl-1.19.4 -y
+  systemctl enable kubelet.service
+
+  info "确认kubelet kubeadmin kubectl是否安装成功"
+  yum list installed | grep kubelet
+  yum list installed | grep kubeadm
+  yum list installed | grep kubectl
+  kubelet --version
+}
+
+function kubeadmin_init() {
+  sleep 1
+  read -p "请输入master ip地址：" ip
+  kubeadm init --apiserver-advertise-address="${ip}" --image-repository registry.aliyuncs.com/google_containers --kubernetes-version v1.19.4 --service-cidr=10.96.0.0/12 --pod-network-cidr=10.244.0.0/16
+  mkdir -p "$HOME"/.kube
+  sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+  sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
 cat > download_image.sh << ERIC
 #!/bin/bash
 images=(
-    kube-apiserver:v1.20.0
-    kube-controller-manager:v1.20.0
-    kube-scheduler:v1.20.0
-    kube-proxy:v1.20.0
+    kube-apiserver:v1.19.4
+    kube-controller-manager:v1.19.4
+    kube-scheduler:v1.19.4
+    kube-proxy:v1.19.4
     pause:3.2
     etcd:3.4.13-0
     coredns:1.7.0
@@ -122,34 +194,17 @@ ERIC
 chmod -R 755 download_image.sh
 ./download_image.sh
 
-## iP replase
-cat > kubeadm-init.yaml << ERIC
-apiVersion: kubeadm.k8s.io/v1beta2
-kind: ClusterConfiguration
-kubernetesVersion: v1.20.0
+}
 
-localAPIEndpoint:
-  advertiseAddress: ${MASTER}
-  bindPort: 6443
-
-networking:
-  dnsDomain: apiserver.cluster.local
-  podSubnet: 10.244.0.0/16
-  serviceSubnet: 10.96.0.0/12
-scheduler: {}
-ERIC
-swapoff -a && sed -ri 's/.*swap.*/#&/' /etc/fstab
-kubeadm init --config kubeadm-init.yaml
-
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-
-
-
-
-######
-docker pull  quay.mirrors.ustc.edu.cn/coreos/flannel:v0.15.1
+function install_flannel() {
+  read -p "是否安装flannel latest？默认为：no. Enter [yes/no]：" github
+  if [[ "$github" == 'yes' ]];
+   then
+     yum -y install wget
+     wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+     kubectl apply -f kube-flannel.yml
+   else
+     docker pull  quay.mirrors.ustc.edu.cn/coreos/flannel:v0.15.1
 docker tag quay.mirrors.ustc.edu.cn/coreos/flannel:v0.15.1 quay.io/coreos/flannel:v0.15.1
 cat > kube-flannel.yaml <<EOF
 ---
@@ -391,11 +446,18 @@ spec:
           name: kube-flannel-cfg
 EOF
 kubectl apply -f kube-flannel.yaml
-kubectl get nodes
-kubectl taint node k8s-master node-role.kubernetes.io/master-
+  fi
 
-### 自动补全命令
-apt install -y bash-completion
-locate bash_completion
-source /usr/share/bash-completion/bash_completion
-source <(kubectl completion bash)
+}
+
+# 安装docker
+read -p "是否安装docker？默认为：no. Enter [yes/no]：" is_docker
+if [[ "$is_docker" == 'yes' ]];then
+  run_function "install_docker"
+fi
+
+# 安装k8s
+read -p "是否安装k8s？默认为：no. Enter [yes/no]：" is_k8s
+if [[ "$is_k8s" == 'yes' ]];then
+  run_function "install_k8s"
+fi
